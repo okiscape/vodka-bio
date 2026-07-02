29355c089697c70a588ae131566b7aa4e16ebb41
okiscape <n3vereverett@gmail.com>
2026-07-02 18:46:18 +0300

    fix(env): added force-dynamic rendering where process.env used

--- apps/frontend/app/admin/page.tsx
+++ apps/frontend/app/admin/page.tsx
@@ -2,11 +2,10 @@ import AdminPanel from "./admin-panel"
 
 import "./admin.css"
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
-	if (!process.env.API_BASEURL) return (
-		<p>envs again, contact me</p>
-	)
   return (
-    <AdminPanel apiBaseUrl={process.env.API_BASEURL} />
+    <AdminPanel apiBaseUrl={process.env.API_BASEURL ?? ''} />
   )
 }
--- apps/frontend/app/ratings/page.tsx
+++ apps/frontend/app/ratings/page.tsx
@@ -1,5 +1,7 @@
 import RatingsList from './ratings-list'
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return <RatingsList apiBaseUrl={process.env.API_BASEURL ?? ''} />
 }
