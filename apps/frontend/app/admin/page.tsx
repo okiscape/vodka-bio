0d8b6b054e33677627d584317ea770237b6a5dba
okiscape <n3vereverett@gmail.com>
2026-07-02 16:34:31 +0300

    fix(env): added env-undefined protection in admin

--- apps/frontend/app/admin/page.tsx
+++ apps/frontend/app/admin/page.tsx
@@ -1,10 +1,11 @@
-export const dynamic = 'force-dynamic'
-
 import AdminPanel from "./admin-panel"
 
 import "./admin.css"
 
 export default function Page() {
+	if (!process.env.API_BASEURL) return (
+		<p>envs again, contact me</p>
+	)
   return (
     <AdminPanel apiBaseUrl={process.env.API_BASEURL} />
   )
