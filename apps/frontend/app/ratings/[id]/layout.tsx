1940110fa6997b216524f22e35dcb8f41d814813
okiscape <n3vereverett@gmail.com>
2026-07-02 15:12:49 +0300

    fix(ratings fetch): added forced dynamic rendering mode

--- apps/frontend/app/ratings/[id]/layout.tsx
+++ apps/frontend/app/ratings/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { getRating } from '../funcs'
 
+export const dynamic = 'force-dynamic'
 export async function generateMetadata(
   { params }: { params: Promise<{ id: string }> }
 ): Promise<Metadata> {
