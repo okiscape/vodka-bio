556aa5a2d597f9159a0127f6e6ca6e2550ea878d
okiscape <n3vereverett@gmail.com>
2026-07-02 18:37:36 +0300

    fix(env): added env in next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
 	env: {
     GITHUB_REPO_URL: process.env.GITHUB_REPO_URL,
 		SENKODIGITAL_REFERRAL: process.env.SENKODIGITAL_REFERRAL,
-    OTORING_SLUG: process.env.OTORING_SLUG,
+		OTORING_SLUG: process.env.OTORING_SLUG,
+    API_BASEURL: process.env.API_BASEURL
   },
 };
 
