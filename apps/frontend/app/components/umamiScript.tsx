5f8b37b533ec7c53def0ab748b6004ca2314f337
okiscape <n3vereverett@gmail.com>
2026-07-17 02:34:45 +0300

    feat(umami): added umami masking via env

--- apps/frontend/app/components/umamiScript.tsx
+++ apps/frontend/app/components/umamiScript.tsx
@@ -6,7 +6,13 @@ type Props = { domain: string, website_id: string }
 export function UmamiScript({domain, website_id}: Props) {
   useEffect(() => {
     const script = document.createElement('script')
-    script.src = `https://umami.${domain}/script.js`
+    script.src = `https://umami.${domain}/metrics.js`
+    script.setAttribute('data-website-id', website_id)
+    script.defer = true
+      document.body.appendChild(script)
+
+    const scriptRecorder = document.createElement('script')
+    script.src = `https://umami.${domain}/recorder.js`
     script.setAttribute('data-website-id', website_id)
     script.defer = true
     document.body.appendChild(script)
