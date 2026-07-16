10f7a73267e68de7daad97f532f805090f4df65a
okiscape <n3vereverett@gmail.com>
2026-07-17 02:38:21 +0300

    fix(umami): fixed metrics endpoint

--- apps/frontend/app/components/umamiScript.tsx
+++ apps/frontend/app/components/umamiScript.tsx
@@ -6,7 +6,7 @@ type Props = { domain: string, website_id: string }
 export function UmamiScript({domain, website_id}: Props) {
   useEffect(() => {
     const script = document.createElement('script')
-    script.src = `https://umami.${domain}/metrics.js`
+    script.src = `https://umami.${domain}/metrics`
     script.setAttribute('data-website-id', website_id)
     script.defer = true
       document.body.appendChild(script)
