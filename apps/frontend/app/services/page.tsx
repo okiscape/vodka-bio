5bcb5afbdf15b25b7d2b8a26ca80d094fd19d0ac
okiscape <n3vereverett@gmail.com>
2026-09-06 02:40:39 +0300

    fix(services, render #1): autoplay fixed(audio removed)

--- apps/frontend/app/services/page.tsx
+++ apps/frontend/app/services/page.tsx
@@ -10,7 +10,7 @@ export default function ServicesPage() {
   return (
     <div className="services">
       <div className="description">
-        <video src="/videos/services.mp4" autoPlay loop className="laptopvideo" />
+        <video src="/videos/services.mp4" autoPlay loop className="laptopvideo"/>
         <div className="text">
           <h1>i can create and support many of things</h1>
         </div>
